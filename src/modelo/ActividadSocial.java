8aeddc37563100233cf2ed5a903e1815de755320
Hielito <fsedenoguerrero@uma.es>
2020-12-07 19:09:22 +0100

    Merge Evento Usuario

--- src/modelo/ActividadSocial.java
+++ src/modelo/ActividadSocial.java
@@ -1,13 +1,18 @@
 package modelo;
-
 import java.util.Date;
 
 public class ActividadSocial extends Evento {
     private String Lugar;
 
-    public ActividadSocial(Date dia, String nom, String lugar) {
-        super(dia,nom);
+
+    public ActividadSocial(int identificador,Date dia, String nom,Usuario dueno,  String lugar) {
+        super(identificador,dia,nom, dueno);
         this.Lugar = lugar;
     }
+
+    public ActividadSocial(int identificador) {
+        super(identificador);
+    }
+
 
 }
--- src/modelo/Evento.java
+++ src/modelo/Evento.java
@@ -1,16 +1,40 @@
 package modelo;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public abstract class Evento {
 
+    private int id;
     private Date Fecha;
     private String Nombre;
+    private List<Usuario> usuarios;
+    private Usuario creador;
     //TODO private Contenido[] contenido;
 
-    public Evento(Date dia, String nom) {
+    public Evento(int identificador,Date dia, String nom, Usuario dueno) {
+        id = identificador;
         Fecha = dia;
         Nombre = nom;
+        usuarios = new ArrayList<>();
+        if (dueno instanceof Tutor || dueno instanceof Colaborador) { //TODO dueno tambien puede ser instancia de colaborador
+            creador = dueno;
+        } else {
+            throw new RuntimeException("El creador debe ser un tutor o un colaborador");
+        }
+    }
+
+    public Evento(int identificador) {
+        id = identificador;
+    }
+
+    public void inscripcionUsuario(Usuario user) {
+        if (usuarios.contains(user)) {
+            throw new RuntimeException("El usuario ya está registrado en el evento");
+        } else {
+            usuarios.add(user);
+        }
     }
 
 }
