4792cc2103521f45a33e88b8cd2969d0062af8a9
franUma <fran30032000@uma.es>
2020-12-07 18:32:08 +0100

    Estudiante

--- src/modelo/Estudiante.java
+++ src/modelo/Estudiante.java
@@ -2,4 +2,11 @@ package modelo;
 
 public class Estudiante extends Usuario{
 
+    public Estudiante(String corr, String dni, String nombr, String apell) {
+        super(corr, dni, nombr, apell);
+    }
+
+    public Estudiante(String corr) {
+        super(corr);
+    }
 }
