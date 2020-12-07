b6d007b05d1a537d2a60b9e25f4dc21db5506555
Hielito <fsedenoguerrero@uma.es>
2020-12-07 19:09:52 +0100

    Constructores Eventos

--- src/modelo/Administrador.java
+++ src/modelo/Administrador.java
@@ -1,6 +1,7 @@
 package modelo;
 
 public class Administrador extends Usuario{
+
     public Administrador(String cor, String dni, String nombr, String apell, String contr) {
         super(cor, dni, nombr, apell, contr);
     }
