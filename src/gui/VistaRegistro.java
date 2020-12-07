252bc47d3a21d758326f92aee61839574b4f5d15
DanielSDVG <danielsdvg@gmail.com>
2020-12-07 18:42:24 +0100

    Creada pantalla "VistaRegistro"

---
+++ src/gui/VistaRegistro.java
@@ -0,0 +1,11 @@
+package gui;
+
+import javax.swing.*;
+
+public class VistaRegistro {
+    private JLabel lblTitulo;
+    private JButton btnEstudiante;
+    private JButton btnTutor;
+    private JButton btnColaborador;
+    private JButton btnAtras;
+}
