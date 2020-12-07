0668ef10d89fb116db7205d5817c01852bd4c442
franUma <fran30032000@uma.es>
2020-12-07 21:14:48 +0100

    BD

--- src/Programa.java
+++ src/Programa.java
@@ -1,4 +1,5 @@
 import gui.VistaBase;
+import modelo.BD;
 
 import javax.swing.*;
 import java.awt.*;
@@ -7,10 +8,17 @@ public class Programa {
 
     private static final String TITULO_VENTANA = "ApSUMA";
     private static final Dimension TAMANIO_VENTANA = new Dimension(800, 450);
+    private static final String databaseName = "jdbc:mysql://" +
+            "ingreq2021-mysql.cobadwnzalab.eu-central-1.rds.amazonaws.com" +
+            ":3306/Aps_UMA";
+    private static final String usr = "grupo10";
+    private static final String pass = "guillermoeduardo2021";
+
 
     public static void main(String[] args) {
         System.out.println("Abriendo ApSUMA...");
-        crearGUI();
+        BD miBD = new BD(databaseName,usr,pass);
+        //crearGUI();
     }
 
     private static void crearGUI() {
