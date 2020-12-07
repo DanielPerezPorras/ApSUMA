43ae483c0605a95762f9d7c52af5f4287aa154e1
Hielito <fsedenoguerrero@uma.es>
2020-12-07 18:07:43 +0100

    Eventos Creados

--- src/modelo/Eventos.java
+++ src/modelo/Eventos.java
@@ -7,4 +7,10 @@ public abstract class Eventos {
     private Date Fecha;
     private String Nombre;
     //TODO private Contenido[] contenido;
+
+    public Eventos(Date dia, String nom) {
+        Fecha = dia;
+        Nombre = nom;
+    }
+
 }
