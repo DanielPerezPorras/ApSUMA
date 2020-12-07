bf6f9ce2c0e5288cbfb9722c2e6c1d4359428004
franUma <fran30032000@uma.es>
2020-12-07 17:49:11 +0100

    Tutor

--- src/modelo/Tutor.java
+++ src/modelo/Tutor.java
@@ -1,5 +1,12 @@
 package modelo;
 
 public class Tutor extends Usuario {
+    private String correoUMA;
+    private Evento[] propuesto;
+    private Usuario[] usuario;
 
+    public Tutor(String cUMA){
+        super();
+        correoUMA = cUMA;
+    }
 }
