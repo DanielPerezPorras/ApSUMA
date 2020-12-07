8af904cdaf834bea67b49336db1fe1b5226f64f0
Manuel <manuleiva123@hotmail.es>
2020-12-07 18:05:53 +0100

    Añadido admin2

--- src/modelo/Administrador.java
+++ src/modelo/Administrador.java
@@ -1,9 +1,5 @@
 package modelo;
 
 public class Administrador extends Usuario{
-
-    public Administrador(String cor, String dni, String nombr, String apell) {
-        super(cor, dni, nombr, apell);
-    }
-
+    int i=0;
 }
