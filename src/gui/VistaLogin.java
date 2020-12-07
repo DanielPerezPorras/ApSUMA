e8217739f1ceb95caf3a8a086f1d111371f8017c
guilleam <guilleam@uma.es>
2020-12-07 18:56:11 +0100

    Primera fase de prueba para la creación de la GUI

--- src/gui/VistaLogin.java
+++ src/gui/VistaLogin.java
@@ -1,21 +1,4 @@
 package gui;
 
-import javax.swing.*;
-import java.awt.event.ActionListener;
-
-/**
- * Vista de la pantalla de login.
- */
-public class VistaLogin extends JPanel {
-
-    private ActionListener controlador;
-
-    public VistaLogin() {
-        add(new JLabel("¡Hola mundo!"));
-    }
-
-    public void asignarControlador(ActionListener listener) {
-        controlador = listener;
-    }
-
+public class VistaLogin {
 }
---
+++ src/gui/pruebaGUI.java
@@ -0,0 +1,9 @@
+package gui;
+
+import javax.swing.*;
+
+public class pruebaGUI {
+    private JPanel pTitulo;
+    private JLabel lTitulo;
+    private JLabel lIcono;
+}
