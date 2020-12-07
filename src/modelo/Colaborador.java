737a0bfb35ce75cc93b4f9c930f785add4b8832d
Hielito <fsedenoguerrero@uma.es>
2020-12-07 19:20:21 +0100

    Colaborador fix

--- src/modelo/Colaborador.java
+++ src/modelo/Colaborador.java
@@ -6,9 +6,9 @@ public abstract class Colaborador extends Usuario{
     private String nombre;
     private ArrayList<Evento> creado;
 
-    public Colaborador(String cor, String dni, String nombr, String apell) {
-        super(cor, dni, nombr, apell);
-            creado = new ArrayList<>();
+    public Colaborador(String cor, String dni, String nombr, String apell, String contr) {
+        super(cor, dni, nombr, apell, contr);
+        creado = new ArrayList<>();
     }
 
     public void crearEvento(Evento evento){
