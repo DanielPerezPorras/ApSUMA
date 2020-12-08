ad4052f6ab1c37505ba6cbbe63c8b8c22199d5ad
GuillermoAguado <guilleam@uma.es>
2020-12-08 19:44:38 +0100

    Vista Registro Tutor y Vista Registro colaborador hecha!!

--- src/gui/VistaRegistroTutor.java
+++ src/gui/VistaRegistroTutor.java
@@ -8,35 +8,33 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPasswordField;
 
 public class VistaRegistroTutor extends JFrame {
 
 	private JPanel panelPrincipal;
 	private JTextField tCorreoUMA;
 	private JTextField tCorreo;
-	private JTextField textField;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VistaRegistroTutor frame = new VistaRegistroTutor();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JTextField tNombre;
+	private JPasswordField passwordField;
+	private JPasswordField passwordConfirmField;
+	private JLabel lbCorreoUMA;
+	private JLabel lbCorreo;
+	private JLabel lbNombreDeUsuario;
+	private JLabel lbContrasea;
+	private JLabel lbConfirmeSuContrasea;
+	private JButton btnAtrs;
+	private JButton btnSiguiente;
 
 	/**
 	 * Create the frame.
 	 */
-	public VistaRegistroTutor() {
+	public VistaRegistroTutor() 
+	{
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 568, 367);
@@ -45,47 +43,73 @@ public class VistaRegistroTutor extends JFrame {
 		setContentPane(panelPrincipal);
 		panelPrincipal.setLayout(null);
 		
-		JLabel lbCorreoUMA = new JLabel("Correo electr\u00F3nico UMA");
+		lbCorreoUMA = new JLabel("Correo electr\u00F3nico UMA");
 		lbCorreoUMA.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 18));
-		lbCorreoUMA.setBounds(44, 13, 212, 24);
+		lbCorreoUMA.setBounds(44, 25, 212, 24);
 		panelPrincipal.add(lbCorreoUMA);
 		
-		JLabel lbCorreo = new JLabel("Correo electr\u00F3nico");
+		lbCorreo = new JLabel("Correo electr\u00F3nico");
 		lbCorreo.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 18));
-		lbCorreo.setBounds(44, 58, 212, 24);
+		lbCorreo.setBounds(44, 75, 212, 24);
 		panelPrincipal.add(lbCorreo);
 		
-		JLabel lbNombreDeUsuario = new JLabel("Nombre de usuario");
+		lbNombreDeUsuario = new JLabel("Nombre de usuario");
 		lbNombreDeUsuario.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 18));
-		lbNombreDeUsuario.setBounds(44, 107, 212, 24);
+		lbNombreDeUsuario.setBounds(44, 124, 212, 24);
 		panelPrincipal.add(lbNombreDeUsuario);
 		
-		JLabel lbContrasea = new JLabel("Contrase\u00F1a");
+		lbContrasea = new JLabel("Contrase\u00F1a");
 		lbContrasea.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 18));
-		lbContrasea.setBounds(44, 162, 212, 24);
+		lbContrasea.setBounds(44, 179, 212, 24);
 		panelPrincipal.add(lbContrasea);
 		
-		JLabel lbConfirmeSuContrasea = new JLabel("Confirme su contrase\u00F1a");
+		lbConfirmeSuContrasea = new JLabel("Confirme su contrase\u00F1a");
 		lbConfirmeSuContrasea.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 18));
-		lbConfirmeSuContrasea.setBounds(44, 219, 212, 24);
+		lbConfirmeSuContrasea.setBounds(44, 236, 212, 24);
 		panelPrincipal.add(lbConfirmeSuContrasea);
 		
 		tCorreoUMA = new JTextField();
 		tCorreoUMA.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 15));
-		tCorreoUMA.setBounds(288, 11, 251, 31);
+		tCorreoUMA.setBounds(288, 23, 251, 31);
 		panelPrincipal.add(tCorreoUMA);
 		tCorreoUMA.setColumns(10);
 		
 		tCorreo = new JTextField();
 		tCorreo.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 15));
 		tCorreo.setColumns(10);
-		tCorreo.setBounds(288, 61, 251, 31);
+		tCorreo.setBounds(288, 73, 251, 31);
 		panelPrincipal.add(tCorreo);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 15));
-		textField.setColumns(10);
-		textField.setBounds(288, 110, 251, 31);
-		panelPrincipal.add(textField);
+		tNombre = new JTextField();
+		tNombre.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 15));
+		tNombre.setColumns(10);
+		tNombre.setBounds(288, 122, 251, 31);
+		panelPrincipal.add(tNombre);
+		
+		btnAtrs = new JButton("Atr\u00E1s");
+		btnAtrs.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 15));
+		btnAtrs.setBounds(12, 295, 118, 25);
+		panelPrincipal.add(btnAtrs);
+		
+		btnSiguiente = new JButton("Registrarse");
+		btnSiguiente.setFont(new Font("Microsoft JhengHei UI", Font.PLAIN, 15));
+		btnSiguiente.setBounds(432, 295, 118, 25);
+		panelPrincipal.add(btnSiguiente);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(288, 178, 251, 31);
+		panelPrincipal.add(passwordField);
+		
+		passwordConfirmField = new JPasswordField();
+		passwordConfirmField.setBounds(288, 239, 251, 31);
+		panelPrincipal.add(passwordConfirmField);
+	}
+	
+	public void controlador(ActionListener ctr)
+	{
+		btnAtrs.addActionListener(ctr);
+		btnAtrs.setActionCommand("ATRAS");
+		btnSiguiente.addActionListener(ctr);
+		btnSiguiente.setActionCommand("REGISTRO TUTOR");
 	}
 }
