a67700f7e5e970bdf72a646db0a0275add43fc74
FranciscoSedeno <fsedenoguerrero@uma.es>
2020-12-08 17:38:53 +0100

    Merge branch 'master' of C:\Users\fsede\Desktop\UMA\IR\APS_UMA with conflicts.

--- src/Usuario.java
+++
@@ -1,9 +0,0 @@
-public abstract class Usuario {
-    private String correo;
-    private String DNI;
-    private String nombre;
-    private String apellido;
-    //private Eventos[] actividad;
-    //private Tutor[] tutor;
-
-}
