1933239fa3ea8fcb0f91a4a200f3e669c6c0c41c
FranciscoSedeno <fsedenoguerrero@uma.es>
2020-12-14 22:31:56 +0100

    VisPerEst dispose on close

--- src/gui/VistaPerfilEstudiante.java
+++ src/gui/VistaPerfilEstudiante.java
@@ -38,7 +38,7 @@ public class VistaPerfilEstudiante extends JFrame {
 	 */
 	public VistaPerfilEstudiante() {
 		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 689, 432);
 		panelPrincipal = new JPanel();
 		panelPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
