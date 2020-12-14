b467f6f63f56b5388526ef53ddc37adf2705baf3
GuillermoAguado <guilleam@uma.es>
2020-12-14 21:32:47 +0100

    Parte Controlador Principal Admin

--- src/gui/ControladorPrincipalAdmin.java
+++ src/gui/ControladorPrincipalAdmin.java
@@ -2,18 +2,46 @@ package gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ControladorPrincipalAdmin implements ActionListener {
+import modelo.BD;
 
-    private final VistaPrincipalAdmin vista;
+public class ControladorPrincipalAdmin implements ActionListener 
+{
 
-    public ControladorPrincipalAdmin(VistaPrincipalAdmin vista) {
-        this.vista = vista;
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
+private final VistaPrincipalAdmin vista;
+	
+	public ControladorPrincipalAdmin(VistaPrincipalAdmin vista)
+	{
+		this.vista = vista;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		BD bd = new BD();
+		switch(e.getActionCommand())
+		{
+			case "BUSCAR":
+				//Busca los usuarios que coincidan con la cadena de texto pedida
+				String busqueda = vista.getTextBoxValue();
+				
+				List<String> listaNombres = new ArrayList<String>();
+				List<Object[]> listaResultado = bd.Select("SELECT * FROM Usuario WHERE nombreUsuario LIKE '%" + busqueda + "%';");
+				for (Object[] objects : listaResultado) 
+				{
+					listaNombres.add((String)objects[0]);
+				}
+				vista.anyadirTexto(listaNombres.toArray(new String[0]));
+				break;
+			case "PENALIZAR":
+				break;
+			case "ELIMINAR":
+				
+				break;
+		}
+		
+	}
 
 }
